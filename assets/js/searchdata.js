6ab3a837b72ff45c09f1cac07c6d9fa3dd931edc
Blue <itzblueman@gmail.com>
2025-04-01 20:46:46 +0200

    Search adjustments

--- assets/js/searchdata.js
+++ assets/js/searchdata.js
@@ -16,6 +16,9 @@ var jsondata=[
   {% endfor %}
   ,
   {% for page in site.html_pages %}
+    {% if page.name == '404.html' %}
+      {% continue %}
+    {% endif %}
    {
      {% assign title = page.title | default: page.name %}
      {% if title != nil %}
@@ -34,7 +37,7 @@ var sjs = SimpleJekyllSearch({
     searchInput: document.getElementById('search-input'),
     resultsContainer: document.getElementById('results-container'),
     json: jsondata,
-    searchResultTemplate: '<li><a href="{url}" title="{desc}">{title}</a></li>',
+    searchResultTemplate: '<li><a href="{url}" title="{title}">{title}</a></li>',
     noResultsText: 'No results found',
     limit: 10,
     fuzzy: false,
